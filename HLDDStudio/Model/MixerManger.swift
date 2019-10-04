9a736c926f2d4f6933b79deab12ee2931ba10ec4
ting hui wu <tinghuiwu@tingde-MacBook-Pro.local>
2019-10-04 13:45:27 +0800

    DrumPattern view ok.

--- HLDDStudio/Model/MixerManger.swift
+++ HLDDStudio/Model/MixerManger.swift
@@ -42,7 +42,6 @@ enum MixerMangerTilte {
     case recording
     
     case finishingRecording
-    
 }
 
 enum MixerMangerSubTilte {
@@ -61,6 +60,7 @@ enum MixerMangerSubTilte {
 }
 
 enum InputSourceCase: String {
+    
     case noInput = "No Input"
     
     case drumMachine = "DrumMachine"
@@ -78,12 +78,14 @@ class MixerManger {
     let semaphore = DispatchSemaphore(value: 0)
     
     var bar = 0 {
+        
         didSet {
             
         }
     }
     
     var beat = 0 {
+        
         didSet {
             
         }
@@ -96,10 +98,6 @@ class MixerManger {
     var metronomeStartTime:AVAudioTime = AVAudioTime.now()
     
     var drumMachineStartTime: AVAudioTime = AVAudioTime.now()
-    
-//    var firstTrackStatus = TrackInputStatus.noInput
-//    
-//    var secondTrackStatus = TrackInputStatus.noInput
     
     var mic: AKMicrophone!
 
@@ -118,13 +116,17 @@ class MixerManger {
     var isenabledMixerFunctionalButton: Bool = true
     
     var titleContent: String = "" {
+        
         didSet {
+            
             NotificationCenter.default.post(.init(name: .mixerNotificationTitleChange))
         }
     }
 
     var subTitleContent: String = ""{
+        
         didSet {
+            
             NotificationCenter.default.post(.init(name: .mixerNotificationSubTitleChange))
         }
     }
@@ -132,16 +134,24 @@ class MixerManger {
     var mixerStatus = MixerStatus.stopRecordingAndPlaying
     
     init() {
+        
         metronome.callback = metronomeCallBack
+        
         metronomeBooster = AKBooster(metronome)
+        
         mic = AKMicrophone()
+        
         recorder = AKClipRecorder(node: mixer)
         //
         let recordResult = Result{try AKAudioFile()}
+        
         switch recordResult {
+            
         case .success(let recordFile):
+            
             self.recordFile = recordFile
         case .failure:
+            
             print(MixerError.recordFileError)
         }
     }
@@ -149,49 +159,69 @@ class MixerManger {
     func metronomeCallBack() {
         
         NotificationCenter.default.post(.init(name: .mixerBarTitleChange))
+        
         drumMachineStartTime = AVAudioTime.now()
         
         if mixerStatus  == .prepareToRecordAndPlay {
+            
             metronomeStartTime = AVAudioTime.now()
             
             mixerStatus = .recordingAndPlaying
+            
             semaphore.signal()
         }
         
-        DispatchQueue.main.async {[weak self] in
-            guard let self = self else{return}
-            self.beat += 1
-            self.bar = Int(self.beat/4)
+        DispatchQueue.main.async { [ weak self ] in
+            
+            guard let strongSelf = self
+                else { return }
+            
+            strongSelf.beat += 1
+            
+            strongSelf.bar = Int(strongSelf.beat/4)
         }
-        
     }
     
     func title(with title: MixerMangerTilte) {
+        
         switch title {
+            
         case .HLDDStudio:
+            
             MixerManger.manger.titleContent = "HLDDStudio"
         case .recordWarning:
+            
             MixerManger.manger.titleContent = "RecordWarning"
         case .finishingRecording:
+            
             MixerManger.manger.titleContent = "Record Complete."
         case .recording:
+            
             MixerManger.manger.titleContent = "Mixer Is Recording. . ."
         }
     }
     
     func subTitle(with subTitle: MixerMangerSubTilte ) {
+        
         switch subTitle {
+            
         case .selectInputDevice:
+            
             MixerManger.manger.subTitleContent = "Select Input Device."
         case .metronomeIsOn:
+            
             MixerManger.manger.subTitleContent = "Metronome Is On."
         case .metronomeIsOff:
+            
             MixerManger.manger.subTitleContent = "Metronome Is Off."
         case .checkInputSource:
+            
             MixerManger.manger.subTitleContent = "Check Input Source Before Recording."
         case .noFileOrInputSource:
+            
             MixerManger.manger.subTitleContent = "No File Is Playing."
         case .barWarning:
+            
             MixerManger.manger.subTitleContent = "Starting Bar Should Less Or Equal Than Ending Bar."
         }
     }
